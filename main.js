fbe56fb74a262984cefdc50d5c058e3da1884dc3
Lojith <lojith@proton.me>
2025-09-19 01:45:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { Game } from './game.js';
 
-const myGame = new Game();
-myGame.createCharacters();
-myGame.run();
+const game = new Game();
+await game.load();     // load assets
+game.start();          // render initial state
