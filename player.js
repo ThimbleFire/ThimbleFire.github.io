05c874caf1ffb4e22331773371e3e8fea1aef9a3
Lojith <lojith@proton.me>
2025-09-19 04:36:47 +0100

    Update player.js

--- player.js
+++ player.js
@@ -1,10 +1,16 @@
 export class Player extends Character {
 
-    constructor(name, x, y, ctx) {
+    constructor(name, x, y, ctx, input) {
         super(name, x, y, ctx)
+        this.input = input
+        this.x = x
+        this.y = y
     }
 
-    handle_input(delta, input) {
-        
+    update(delta) {
+        const movement = this.input.movementVector;
+
+        this.x += movement.x * delta * 0.1;
+        this.y += movement.y * delta * 0.1;
     }
 }
