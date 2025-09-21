7eccb571cab74c611b7ad452573fb896de13d65f
AlwaysEast <lojith@proton.me>
2025-09-21 21:56:40 +0100

    mhmm

--- space/README.md
+++
@@ -1,3 +0,0 @@
-todo
-
-make a list of interact tiles which point toward modules, since some modules are bigger than just 1x1
--- space/main.js
+++
@@ -1,5 +0,0 @@
-import { Game } from './game.js';
-
-const game = new Game();
-await game.load('./mapdata/station');     // load assets
-game.start();          // render initial state
