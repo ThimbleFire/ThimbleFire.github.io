cafb92aa73381722eae6f86968b7096bcf7fc6b1
Lojith <lojith@proton.me>
2025-10-09 03:29:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const pages = {
     home: `<h1>Patch Notes</h1>`,
-    download: `<h1>Download</h1><p>It's not out yet but it'll be available for Windows and Linux. I don't have any plans to ever release this game for MacOS.</p>`,
-    about: `<h1>About</h1><p>Generic survival sandbox. </br>Forage, craft, explore, build, gather, manufacture, trade, claim, expand, make alliances, wage wars, conquer.</p>
-    <p>Reldawin is permadeath. Permadeath can be bullshit. To make it not suck items are common and recipes don't ask for much. But make no mistake, your skills and equipment condition will massively improve your odds at surviving winters, wars and wilderness.`,
-    support: `<h1>Support</h1><p>Found a bug? Email me at Lojith@Reldawin.com.</br>I don't use social media, so. No links to share.</p>`
+    play: `lol jk`,
+    about: `<h1>About</h1><p>Reldawin is a permadeath survival MMO that runs in the browser.</p><p>In Reldawin characters do not have names. In place of a friends list there is an acquaintance list where characters can be added and given names and colour flags.</p>`,
+    support: `<h1>Contact</h1><p>Found a bug? IDK how when the game isn't out yet. Time travellers can email me at Lojith@Reldawin.com.</p>`
 };
 
 async function loadPatchNotes() {
