462cd37ef9fca9ba0c9b660d72ef2197e85eabba
Lojith <lojith@proton.me>
2025-10-17 03:01:13 +0100

    Remove argument from game.load() call

--- other/main.js
+++ other/main.js
@@ -1,5 +1,5 @@
 import { Game } from './game.js';
 
 const game = new Game();
-await game.load('./mapdata/station');     // load assets
+await game.load();     // load assets
 game.start();          // render initial state
